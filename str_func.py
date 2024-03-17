a939fca227f8af317d56f2cb756f6a49bb6cb5db
Ramil Nigamatulin <i@rnigamatulin.ru>
2024-03-17 22:51:42 +0500

    upper world

--- str_func.py
+++ str_func.py
@@ -1,9 +1,11 @@
 def titlecase(x):
     print(x.title())
 
-titlecase("ghb jkkmmk jkkll ooim")
+titlecase("ghb jkkmmk jkkll ooijm")
 
 def upper_world(s):
     print(s.upper())
 
-upper_world("hfbsh hhfjfj jksiwkk jkedek")
+upper_world("hfbsh hhfjfj jksiwkk jkedekk")
+
+
